623e567d57ce469a778bf0a904ab4594121c8e48
Steph Barden <stephyx.web@gmail.com>
2019-02-10 01:32:55 +0000

    tidy style

--- webcam/js/heck.js
+++ webcam/js/heck.js
@@ -6,23 +6,23 @@ function startTimers() {
    var screen1 = setTimeout(function(){
       console.log('screen1 change works');
       document.getElementById('section1').style.display = 'none';
-      document.getElementById('section2').style.display = 'block';}, 10000);
+      document.getElementById('section2').style.display = 'block';}, 5000);
 
    var screen2 = setTimeout(function () {
       console.log('screen2 change works');
       document.getElementById('section2').style.display = 'none';
-      document.getElementById('section3').style.display = 'block';}, 20000);
+      document.getElementById('section3').style.display = 'block';}, 10000);
 
    var screen3 = setTimeout(function section_change3(screen3) {
       console.log('screen3 change works');
       document.getElementById('section3').style.display = 'none';
-      document.getElementById('section4').style.display = 'block';}, 30000);
+      document.getElementById('section4').style.display = 'block';}, 15000);
 
-   var screen4 = setTimeout(function section_change4(screen4) {
+   var screen4 = setTimeout(function section_change3(screen4) {
       console.log('screen4 change works');
       document.getElementById('section4').style.display = 'none';
-      // document.getElementById('main').style.display = 'block';
-   }, 40000);
+      document.getElementById('section5').style.display = 'block';}, 20000);
+
 }
 
 function change_text() {
