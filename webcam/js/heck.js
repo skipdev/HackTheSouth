7fc1c27fcc14a955ba8c8a5239fe04eb872cb59c
Steph Barden <stephyx.web@gmail.com>
2019-02-10 01:21:47 +0000

    basic app

---
+++ webcam/js/heck.js
@@ -0,0 +1,30 @@
+function onLoad() {
+   startTimers();
+}
+
+function startTimers() {
+   var screen1 = setTimeout(function(){
+      console.log('screen1 change works');
+      document.getElementById('section1').style.display = 'none';
+      document.getElementById('section2').style.display = 'block';}, 10000);
+
+   var screen2 = setTimeout(function () {
+      console.log('screen2 change works');
+      document.getElementById('section2').style.display = 'none';
+      document.getElementById('section3').style.display = 'block';}, 20000);
+
+   var screen3 = setTimeout(function section_change3(screen3) {
+      console.log('screen3 change works');
+      document.getElementById('section3').style.display = 'none';
+      document.getElementById('section4').style.display = 'block';}, 30000);
+
+   var screen4 = setTimeout(function section_change4(screen4) {
+      console.log('screen4 change works');
+      document.getElementById('section4').style.display = 'none';
+      // document.getElementById('main').style.display = 'block';
+   }, 40000);
+}
+
+function change_text() {
+   document.getElementById('text2speech').innerHTML = "yeeeeeet";
+}
